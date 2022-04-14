9603bcbbffeac980759ade6070476638ef0bbd8b
Nikita Teterin <nekitonum@gmail.com>
2022-04-14 11:09:42 +0300

    implement descriptor factory and unit tests

--- hw4/tests/test_meta.py
+++ hw4/tests/test_meta.py
@@ -1,5 +1,12 @@
+'''
+Unit tests for custom metaclass factory
+Assertions are made on attribute access, their names and values
+'''
+from itertools import chain
+
 import pytest
-from utils.meta import PrefixMeta, metaclass_factory
+
+from utils.meta import metaclass_factory
 
 
 def test_faker(faker):
@@ -21,9 +28,12 @@ def prefix():
     return "custom_"
 
 
-def test_PrefixMeta(faker, prefix):
-    is_dunder = lambda x: x.startswith("__") and x.endswith("__")
+@pytest.fixture
+def is_dunder():
+    return lambda x: x.startswith("__") and x.endswith("__")
 
+
+def test_PrefixMeta(faker, prefix, is_dunder):
     bases = ()
     name = "_".join(faker.name().split())
     attrs = {"".join(faker.name().split()): faker.email() for _ in range(100)}
@@ -38,3 +48,53 @@ def test_PrefixMeta(faker, prefix):
             assert attr.startswith(prefix)
             *_, attr = attr.partition(prefix)
         assert attr in attrs.keys()
+
+
+def test_attribute_access(prefix, is_dunder):
+    transform = lambda a: prefix + a
+    sample_metaclass = metaclass_factory(transform=transform)
+
+    class SampleClass:
+        x = 0
+        y = 10
+        __private = "private attribute"
+        _protected = "protected attribute"
+
+        def __init__(self) -> None:
+            pass
+
+        def some_method(self):
+            return "some_value"
+
+    original_attributes = [attr for attr in dir(SampleClass) if not is_dunder(attr)]
+    magic_attributes = [attr for attr in dir(SampleClass) if is_dunder(attr)]
+
+    class SampleClass(metaclass=sample_metaclass):
+        x = 0
+        y = 10
+        __private = "private attribute"
+        _protected = "protected attribute"
+
+        def __init__(self) -> None:
+            pass
+
+        def some_method(self):
+            return "expected_value"
+
+    instance = SampleClass()
+
+    for attr in chain(magic_attributes, map(transform, original_attributes)):
+        assert hasattr(SampleClass, attr)
+        assert hasattr(instance, attr)
+
+    for attr in chain(original_attributes, map(transform, magic_attributes)):
+        with pytest.raises(AttributeError) as exc_info:
+            getattr(instance, attr)
+        assert isinstance(exc_info.value, AttributeError)
+
+    with pytest.raises(AttributeError) as exc_info:
+        instance.some_method()
+    assert isinstance(exc_info.value, AttributeError)
+
+    bound_method = getattr(instance, transform("some_method"))
+    assert bound_method() == "expected_value"
---
+++ hw4/utils/descriptor.py
@@ -0,0 +1,85 @@
+def type_hint(expected_type):
+    def hint(value):
+        if isinstance(value, expected_type): return
+        raise TypeError(f"Invalid type: expected {expected_type}, got {type(value)}")
+
+    return hint
+
+
+def typed_descriptor(expected_type: type):
+    """
+    Create typed descriptor template for a given type
+
+    Args:
+
+    + `expected_type`: class to ensure the instances of
+    """
+    if type(expected_type) is not type:
+        raise TypeError(f"Expected a class, got {type(expected_type)}")
+
+    # first empty (self) argument is required to bind as a method
+    # original version of this function is left in the outer score
+    # as a quick helper
+    def make_type_hint(expected_type):
+        def hint(_, value):
+            if isinstance(value, expected_type): return
+            raise TypeError(f"Invalid type: expected {expected_type}, got {type(value)}")
+
+        return hint
+
+    class Descriptor:
+        """
+        Descriptor class is a thin wrapper
+        over the original value, ensuring the type inside __set__/__init__ call
+
+        Note that for more complicated descriptors,
+        one should also override the constructor and validate the inputs
+        """
+        __value: expected_type
+        __type_hint = make_type_hint(expected_type)
+
+        def __init__(self, default_value=expected_type()) -> None:
+            self.__type_hint(default_value)
+            self.__value = default_value
+
+        def __get__(self, obj, type=None):
+            return self.__value
+
+        def __set__(self, obj, newvalue):
+            self.__type_hint(newvalue)
+            self.__value = newvalue
+
+    return Descriptor
+
+
+class String(typed_descriptor(str)):
+    """
+    String descriptor
+    """
+
+
+class Integer(typed_descriptor(int)):
+    """
+    Integer descriptor
+    """
+
+
+class PositiveInteger(Integer):
+    """
+    Positive integer descriptor
+    """
+    @staticmethod
+    def __value_hint(newvalue):
+        if newvalue >= 0: return
+        raise ValueError(f"Integer provided must be positive, got {newvalue}")
+
+
+    # note the overriden __init__ and __call__ methods
+    def __set__(self, obj, newvalue):
+        super().__set__(obj, newvalue)
+        PositiveInteger.__value_hint(newvalue)
+
+
+    def __init__(self, default_value=0) -> None:
+        super().__init__(default_value)
+        PositiveInteger.__value_hint(default_value)
