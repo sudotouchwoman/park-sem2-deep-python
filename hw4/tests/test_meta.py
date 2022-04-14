aed27042fdb2d360dbd46196400e7abe9661e0f2
Nikita Teterin <nekitonum@gmail.com>
2022-04-14 08:30:53 +0300

    write tests for metaclass factory

--- hw4/tests/test_meta.py
+++ hw4/tests/test_meta.py
@@ -1,6 +1,40 @@
 import pytest
-from utils.meta import PrefixMeta, make_custom_metaclass
+from utils.meta import PrefixMeta, metaclass_factory
 
-class SampleClass(metaclass=PrefixMeta):
-    def __init__(self) -> None:
+
+def test_faker(faker):
+    assert isinstance(faker.name(), str)
+
+
+def test_meta():
+    sample_metaclass = metaclass_factory()
+
+    class SampleClass(metaclass=sample_metaclass):
         pass
+
+    assert isinstance(sample_metaclass, type)
+    assert isinstance(SampleClass, type)
+
+
+@pytest.fixture
+def prefix():
+    return "custom_"
+
+
+def test_PrefixMeta(faker, prefix):
+    is_dunder = lambda x: x.startswith("__") and x.endswith("__")
+
+    bases = ()
+    name = "_".join(faker.name().split())
+    attrs = {"".join(faker.name().split()): faker.email() for _ in range(100)}
+    attrs.update({item: None for item in dir(faker)})
+
+    sample_metaclass = metaclass_factory(transform=lambda a: prefix + a)
+    SampleClass = sample_metaclass(name, bases, attrs)
+
+    for attr in dir(SampleClass):
+        assert isinstance(attr, str)
+        if not is_dunder(attr):
+            assert attr.startswith(prefix)
+            *_, attr = attr.partition(prefix)
+        assert attr in attrs.keys()
--- hw4/utils/meta.py
+++ hw4/utils/meta.py
@@ -2,7 +2,7 @@ from typing_extensions import Self
 from typing import Any, Callable, Dict
 
 
-def make_custom_metaclass(transform: Callable = None, value_transform: Callable = None):
+def metaclass_factory(transform: Callable = None, value_transform: Callable = None):
     is_dunder_method = lambda attr: attr.startswith("__") and attr.endswith("__")
     _transform = _value_transform = lambda x: x
 
@@ -13,7 +13,7 @@ def make_custom_metaclass(transform: Callable = None, value_transform: Callable 
 
     class Meta:
         def __new__(
-            cls: type[Self], name: str, bases: Any, attrs: Dict[str, Any]
+            cls, name: str, bases: Any, attrs: Dict[str, Any]
         ) -> Self:
             custom_attrs = {
                 _transform(key): _value_transform(value) for key, value in attrs.items()
@@ -23,4 +23,4 @@ def make_custom_metaclass(transform: Callable = None, value_transform: Callable 
     return Meta
 
 
-PrefixMeta = make_custom_metaclass(transform=lambda x: "custom_" + x)
+PrefixMeta = metaclass_factory(transform=lambda x: "custom_" + x)
