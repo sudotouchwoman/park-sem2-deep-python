0584541010933450709c9c19888743f3a63fc121
Nikita Teterin <nekitonum@gmail.com>
2022-05-03 22:17:33 +0300

    add type validation + corresponding test

--- hw5/tests/test_cache.py
+++ hw5/tests/test_cache.py
@@ -1,7 +1,24 @@
+from typing import Tuple, Union
+
+import pytest
+
 from utils import LRUCache
 
 
+@pytest.fixture
+def invalid_limits() -> Tuple[Union[str, int, float]]:
+    return "must be positive", -1, 0, 3.1415
+
+
+def test_cache_creation(invalid_limits: Tuple[Union[str, int, float]]):
+    for limit in invalid_limits:
+        with pytest.raises(ValueError) as exc_info:
+            LRUCache(limit=limit)
+        assert isinstance(exc_info.value, ValueError)
+
+
 def test_cache():
+    # simple test from the task description
     cache = LRUCache(limit=2)
     cache["k1"] = "val1"
     cache["k2"] = "val2"
@@ -10,6 +27,8 @@ def test_cache():
     assert cache["k2"] == "val2"
     assert cache["k1"] == "val1"
 
+    # when extra value is set to cache,
+    # the LRU item ("k2" in this example) is kicked out
     cache["k3"] = "val3"
 
     assert cache["k3"] == "val3"
--- hw5/utils/lru.py
+++ hw5/utils/lru.py
@@ -3,10 +3,16 @@ from typing import Any, Dict, Hashable, Optional
 
 class LRUCache:
     def __init__(self, limit: int) -> None:
+        self._validate_input(limit)
         self.limit: int = limit
         self.timestamp = 0
         self.cache: Dict[Hashable, Any] = {}
         self.lru: Dict[Hashable, int] = {}
+
+    def _validate_input(self, limit: int) -> None:
+        if isinstance(limit, int) and limit > 0:
+            return
+        raise ValueError(f"{limit} should be a positive integer")
 
     def __getitem__(self, key: Hashable) -> Optional[Any]:
         if key in self.cache:
